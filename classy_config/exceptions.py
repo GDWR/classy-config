69192017bdbced2b2e78b645409f2fae1001691a
GDWR <gregory.dwr@gmail.com>
2022-01-22 16:22:04 +0000

    Add docstrings

--- classy_config/exceptions.py
+++ classy_config/exceptions.py
@@ -1,6 +1,19 @@
-class DoubleCreation(BaseException):
-    ...
+class ClassyConfigException(BaseException):
+    """
+    All ClassyConfig exceptions inherit from this exception.
+
+        This is to allow a generic exception handler for
+        any exception that could be raised from this module.
+    """
 
 
-class InstanceNotCreated(BaseException):
-    ...
+class DoubleCreation(ClassyConfigException):
+    """
+    Raised when attempting to create the global context for the second time
+    """
+
+
+class InstanceNotCreated(ClassyConfigException):
+    """
+    Raised when attempting to get a ConfigParam before creating the global context
+    """
