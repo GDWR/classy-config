53f3d60a401802ef7bc44747df5d767246794d8f
GDWR <gregory.dwr@gmail.com>
2022-02-14 10:31:52 +0000

    Remove BaseModel from reference docs

--- classy_config/__init__.py
+++ classy_config/__init__.py
@@ -1,10 +1,7 @@
-from pydantic import BaseModel
-
 from .config import register_config, ConfigValue
 from .loader.auto_loader import register_loader
 
 __all__ = [
-    "BaseModel",
     "ConfigValue",
     "register_config",
     "register_loader",
