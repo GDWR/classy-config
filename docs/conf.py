1661283a95b6650dd082e9684a831194b7ce0cf2
GDWR <gregory.dwr@gmail.com>
2022-02-14 10:32:01 +0000

    Add logo to docs

--- docs/conf.py
+++ docs/conf.py
@@ -55,3 +55,4 @@ html_theme = "furo"
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ['_static']
 html_favicon = 'favicon.ico'
+html_logo = "favicon.ico"
