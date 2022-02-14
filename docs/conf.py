162a77f3ec2f72ad0e0b95e13e4bccea697f65d1
GDWR <gregory.dwr@gmail.com>
2022-02-14 09:38:29 +0000

    Add favicon to documentation

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,6 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.coverage',
     'sphinx.ext.napoleon',
-    'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -55,3 +54,4 @@ html_theme = "furo"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ['_static']
+html_favicon = 'favicon.ico'
