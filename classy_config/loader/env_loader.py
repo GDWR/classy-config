e4bac437ab8cd6adc69daa5c82429f603bb070f0
fisher60 <wakefollower@gmail.com>
2022-05-06 13:46:07 -0500

    fix redundant env var loading
    I have added the env var loader as well as tests. The loader will not override any system environment variables, so system env vars are prioritized over the ones passed into the loader. This is consistent with python-dotenv's methods.
    
    Currently tests cover that env vars in a .env file that is passed to the autoloader will load the correct values with the correct names. The tests do not ensure that correct env vars from the system are loaded in. The test also does not cover whether multiple configs can be loaded together using this method, but those tests should be covered elsewhere and I do not expect the env loader to interact with multiple configs in a breaking manner.

--- classy_config/loader/env_loader.py
+++ classy_config/loader/env_loader.py
@@ -9,15 +9,13 @@ def env_loader(filepath: Path) ->  MutableMapping[str, Any]:
     """
     Load the given *.env file and the user's environment variables.
 
-    Prioritizes system/user environment variables and will overwrite environment variables from the given filepath if
-    a duplicate key exists in the system/user environment variables.
+    Prioritizes system/user environment variables. Will not override any existing environment variables from the
+    system.
 
     :param filepath: The path of the file to load from.
     :return: The mapping of keys to value loaded from the env file and user's environment.
     """
     if filepath:
         load_dotenv(filepath)
-    
-    load_dotenv()
 
     return environ
